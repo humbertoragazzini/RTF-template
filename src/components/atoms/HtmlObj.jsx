a090ef435d7619c2a8abc807a219afa1b4272bd9
humbertoragazzini <beto_vicio@hotmail.com>
2025-05-15 12:08:03 +0000

    added rotation and translation with gimball to html

--- src/components/atoms/HtmlObj.jsx
+++ src/components/atoms/HtmlObj.jsx
@@ -1,13 +1,17 @@
 import { Html } from "@react-three/drei";
+import Gimbal from "../molecules/Gimball";
 
-export default function HtmlObj({position,occlude,children}){
-    return(
-        <mesh position={position}>
-            <Html occlude={occlude} transform>
-                {children}
-            </Html>
-            <planeGeometry args={[1.6,0.9]}></planeGeometry>
-            <meshBasicMaterial></meshBasicMaterial>
-        </mesh>
+export default function HtmlObj({ position, occlude, children }) {
+    return (
+        <Gimbal position={{ x: position[0], y: position[1], z: position[2] }} rotation={{ x: 0, y: 0, z: 0 }} name={"The-div"}>
+            <mesh>
+                <Html occlude={occlude} transform>
+                    {children}
+                </Html>
+                <planeGeometry args={[1.6, 0.9]}></planeGeometry>
+                <meshBasicMaterial></meshBasicMaterial>
+            </mesh>
+        </Gimbal>
+
     )
 }
